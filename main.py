df57e41ac84d7a1a2da5833845be9e54899144e3
omkar <omkarsp20@gmail.com>
2025-01-16 16:30:22 +0530

    Commited

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from fun import factorial
+
 print("Hello world") # from default branch master
 
 print("Welcome to hello world")  # from member1 branch
 
+n1 = int(input("Enter any number :- "))
+print(f"Factorial of {n1} is :- {factorial(n1)}")
+
+print("Modified")
+
+print("Mofie ")
